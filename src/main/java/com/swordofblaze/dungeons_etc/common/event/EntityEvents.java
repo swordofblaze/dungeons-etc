adc508e2bfcf864194cd343602fe8eeda4c76794
Sarinsa <mikkemusbruker@gmail.com>
2020-12-02 14:20:49 +0100

    Added stress relief and infliction upon dealing and receiving critical hits.

--- src/main/java/com/swordofblaze/dungeons_etc/common/event/EntityEvents.java
+++ src/main/java/com/swordofblaze/dungeons_etc/common/event/EntityEvents.java
@@ -3,12 +3,16 @@ package com.swordofblaze.dungeons_etc.common.event;
 import com.swordofblaze.dungeons_etc.common.core.DungeonsEtc;
 import com.swordofblaze.dungeons_etc.common.registers.ModEffects;
 import com.swordofblaze.dungeons_etc.common.tags.ModEntityTypeTags;
+import com.swordofblaze.dungeons_etc.common.util.CapabilityHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -32,4 +36,16 @@ public class EntityEvents {
         }
     }
 
+    @SubscribeEvent
+    public static void onCriticalHit(CriticalHitEvent event) {
+        if (!event.getPlayer().getEntityWorld().isRemote && event.isVanillaCritical()) {
+
+            if (event.getTarget() instanceof PlayerEntity) {
+                    ServerPlayerEntity target = (ServerPlayerEntity) event.getTarget();
+                    CapabilityHelper.addPlayerStress(target, 15);
+            }
+            ServerPlayerEntity playerEntity = (ServerPlayerEntity) event.getPlayer();
+            CapabilityHelper.subtractPlayerStress(playerEntity, 5);
+        }
+    }
 }
